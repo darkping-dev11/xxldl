bff5d267937b5a9a58b8f0175a6cd196585377ca
darkping-dev11 <omerasargd@gmail.com>
2026-03-15 21:55:32 +0300

    Add Profile route to routes.js
    Added a new route for the Profile component.

--- js/routes.js
+++ js/routes.js
@@ -2,10 +2,12 @@ import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 import Submit from './pages/Submit.js';
+import Profile from './pages/Profile.js'; // yeni ekleme
 
 export default [
-{ path: '/', component: List },
-{ path: '/leaderboard', component: Leaderboard },
-{ path: '/roulette', component: Roulette },
-{ path: '/submit', component: Submit },
+  { path: '/', component: List },
+  { path: '/leaderboard', component: Leaderboard },
+  { path: '/roulette', component: Roulette },
+  { path: '/submit', component: Submit },
+  { path: '/profile', component: Profile }, // login sonrası gidilecek sayfa
 ];
