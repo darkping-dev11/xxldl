7c201418d594de2de688ad6b409e560d801627df
darkping-dev11 <omerasargd@gmail.com>
2026-03-15 23:32:20 +0300

    Update Register.js

--- js/pages/Register.js
+++ js/pages/Register.js
@@ -1,13 +1,13 @@
 export default {
   template: `
-    <div class="auth-page register-page">
+    <div class="auth-page">
       <div class="auth-container">
         <h1>Register</h1>
         <p>Create your account to submit records.</p>
         <form @submit.prevent="submit">
-          <input type="text" placeholder="Username" v-model="username" required />
-          <input type="email" placeholder="Email" v-model="email" required />
-          <input type="password" placeholder="Password" v-model="password" required />
+          <input type="text" v-model="username" placeholder="Username" required />
+          <input type="email" v-model="email" placeholder="Email" required />
+          <input type="password" v-model="password" placeholder="Password" required />
           <button type="submit">Register</button>
         </form>
         <p class="auth-footer">
@@ -16,16 +16,10 @@ export default {
       </div>
     </div>
   `,
-  data() {
-    return {
-      username: '',
-      email: '',
-      password: ''
-    }
-  },
+  data() { return { username: '', email: '', password: '' }; },
   methods: {
     submit() {
-      alert('Register submitted! (Backend eklenecek)');
+      alert('Register submitted! (backend eklenecek)');
       this.$router.push('/login');
     }
   }
