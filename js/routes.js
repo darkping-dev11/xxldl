10b98a20f826ec447e0160a07ecca73a7750d61f
darkping-dev11 <omerasargd@gmail.com>
2026-03-15 21:57:12 +0300

    Add Register and Login routes to routes.js

--- js/routes.js
+++ js/routes.js
@@ -2,12 +2,16 @@ import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 import Submit from './pages/Submit.js';
-import Profile from './pages/Profile.js'; // yeni ekleme
+import Register from './pages/Register.js';
+import Login from './pages/Login.js';
+import Profile from './pages/Profile.js';
 
 export default [
   { path: '/', component: List },
   { path: '/leaderboard', component: Leaderboard },
   { path: '/roulette', component: Roulette },
   { path: '/submit', component: Submit },
-  { path: '/profile', component: Profile }, // login sonrası gidilecek sayfa
+  { path: '/register', component: Register },
+  { path: '/login', component: Login },
+  { path: '/profile', component: Profile },
 ];
